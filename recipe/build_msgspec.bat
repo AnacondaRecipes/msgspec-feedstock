5d6443885419fa81ca505aadc75cd071cffef6e5
vchychy <vchychyrko@anaconda.com>
2025-12-18 03:16:02 +0200

    init

--- recipe/build_msgspec.bat
+++ recipe/build_msgspec.bat
@@ -1,2 +1,6 @@
+@echo on
+REM create missing _version.py for PEP517 build without scm metadata
+echo __version__ = "{{ version }}" > src\msgspec\_version.py
+
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation --ignore-installed
 if errorlevel 1 exit 1
--- recipe/build_msgspec.sh
+++ recipe/build_msgspec.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# sdist comes from GITHUB so that setuptools-scm couldn't generate _version.py (no .git)
+# In case of PyPi sdist, setuptools-scm works correct and the line below not needed
+echo "__version__ = \"{{ version }}\"" > src/msgspec/_version.py
+
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation --ignore-installed
